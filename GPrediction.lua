b28103367399fac6c0f935f8e8739ccef88e74ee
KeVuong <hoatn22@gmail.com>
2016-05-09 12:49:51 +0700

    Update GPrediction.lua

--- GPrediction.lua
+++ GPrediction.lua
@@ -1,1 +1,22 @@
 --Kappa
+
+--[[
+Usage
+
+GPrediction:GetPrediction(Target,From,SpellData,Collision)
+Input:
+	target:
+	from:   
+	spelldata: 	
+	      .range
+				.delay
+				.speed
+				.radius/angle	
+				.aoe (true/false)
+				.type ("position","line","circular","cone")
+	
+	collision {"champion","minion","yasuowall"} 
+Output:
+	{CastPosition,Position,HitChance,MaxHit}
+	
+]]
