682a9ac803a35090b1828bd946e9e31e1643bc3b
KeVuong <hoatn22@gmail.com>
2016-08-19 21:52:14 +0700

    Update Doc

--- Common/GPrediction.lua
+++ Common/GPrediction.lua
@@ -4,15 +4,15 @@
 gPred:GetPrediction(target,from,spellData,aoe,checkcol)
 
 spellData	
-    .speed --spell's speed
-		.range  -- spell's range
-		.delay -- spell's delay
-		.radius --spell's radius = width/2
-		.type = "line"/"circular"/"cone" --spell type
-		.col = {"minion","champion","yasuowall"}--table of collision objects
-		.from (range check from, dont use)
+    	.speed --spell's speed
+	.range  -- spell's range
+	.delay -- spell's delay
+	.radius --spell's radius = width/2
+	.type = "line"/"circular"/"cone" --spell type
+	.col = {"minion","champion","yasuowall"}--table of collision objects
+	.from (range check from, dont use)
 			
-from -- Cast From
+from -- where spell casted
 
 aoe(can be nil)		true/false  -- is aoe spell?
 
@@ -22,11 +22,11 @@ checkcol(can be nil) 	true/false -- check collision
 Usage: 
 1. local result = gPred:GetPrediction(target,from,spellData,aoe,checkcol) -- get normal prediction
 
-	result  	
-	    .HitChance 
-			.CastPosition
-			.Position
-			.MaxHit (for AOE spells only)
+result  	
+    	.HitChance 
+	.CastPosition
+	.Position
+	.MaxHit (for AOE spells only)
 
 2. local dashinfo = gPred:GetDashInfo(unit)-- Get unit dash info
 	.isDashing
